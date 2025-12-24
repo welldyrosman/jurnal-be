a26084386cd1f440effdbb54b7db77e968596346
welldyrosman <welldyrosman@gmail.com>
2025-12-24 13:36:52 +0700

    buang factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,21 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Admin MI',
-            'email' => 'admin@mi.com',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'admin@mi.com'],
+            [
+                'name' => 'Admin MI',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ]
+        );
     }
 }
