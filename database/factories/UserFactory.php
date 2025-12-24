cf0c0b854d6e3f69e221d99d9b70ad4df1439692
welldyrosman <welldyrosman@gmail.com>
2025-12-24 13:34:56 +0700

    update lagi aneh

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User; // ⬅️ WAJIB
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -12,14 +13,17 @@ use Illuminate\Support\Str;
 class UserFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     */
+    protected $model = User::class; // ⬅️ INI KUNCINYA
+
+    /**
      * The current password being used by the factory.
      */
     protected static ?string $password;
 
     /**
      * Define the model's default state.
-     *
-     * @return array<string, mixed>
      */
     public function definition(): array
     {
